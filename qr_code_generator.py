270a4af33346fb1c81ef07a19b3a3b16297ca0f3
Saif Al-Jilani <jilanisaif2@gmail.com>
2023-04-04 08:53:07 +0300

    Make it accept image from local

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageOps
 import requests
 from io import BytesIO
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 
 
 color_options = ["black", "red", "green", "blue", "purple"]
@@ -22,6 +22,17 @@ img_url_label = tk.Label(root, text="Image URL:")
 img_url_label.pack()
 img_url_entry = tk.Entry(root)
 img_url_entry.pack()
+
+browse_button = tk.Button(root, text="Browse", command=lambda: browse_file(img_url_entry))
+browse_button.pack()
+
+
+def browse_file(entry):
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.png")])
+    if file_path:
+        entry.delete(0, tk.END)
+        entry.insert(0, file_path)
+
 
 website_url_label = tk.Label(root, text="Website URL:")
 website_url_label.pack()
@@ -43,11 +54,16 @@ def generate_qr():
     website_url = website_url_entry.get()
     qr_color = color_var.get()
     
-   
-    response = requests.get(img_url)
-    img = Image.open(BytesIO(response.content)).convert("RGBA")
+    if not img_url or not website_url:
+        return
+        
+    if img_url.startswith("http"):
+        response = requests.get(img_url)
+        img = Image.open(BytesIO(response.content)).convert("RGBA")
+    else:
+        img = Image.open(img_url).convert("RGBA")
 
-  
+   
     img = img.resize((90, 90))
     img = ImageOps.fit(img, (100, 100), method=Image.LANCZOS)
 
@@ -65,7 +81,18 @@ def generate_qr():
     qr_img.show()
 
 
-generate_button = tk.Button(root, text="Generate QR Code", command=generate_qr)
+generate_button = tk.Button(root, text="Generate QR Code", command=generate_qr, state="disabled")
 generate_button.pack()
+
+
+def check_fields(*args):
+    if img_url_entry.get() and website_url_entry.get():
+        generate_button.config(state="normal")
+    else:
+        generate_button.config(state="disabled")
+
+img_url_entry.bind("<KeyRelease>", check_fields)
+website_url_entry.bind("<KeyRelease>", check_fields)
+
 
 root.mainloop()
